e5395d4588c4cd7052fe89b45bec42281483c3c1
ArmanCS <armanafzali31@gmail.com>
2025-01-16 00:39:49 +0330

    fix category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,8 +22,8 @@ class Category extends Model
         return $this->hasMany(Category::class);
     }
 
-    public function product()
+    public function products()
     {
-        return $this->hasOne(Product::class);
+        return $this->hasMany(Product::class, 'category_id');
     }
 }
